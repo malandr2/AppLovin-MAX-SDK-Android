701eac76e2b0b905561eb833e3dbbbf2aad67418
Justin Malandruccolo <jmalandruccolo@google.com>
2026-06-17 19:28:23 -0400

    Add GMA Next-Gen SDK adapters for AdMob / Ad Manager

---
+++ GoogleAdManagerNextGen/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("adapter-config")
+    id("com.applovin.mobile.publish")
+}
+
+val libraryVersionName by extra("25.3.0.0")
+val minAppLovinSdkVersion by extra("13.0.0")
+
+applovinMobilePublish {
+    libraryArtifactId.set("google-ad-manager-adapter")
+}
+
+android.defaultConfig.minSdk = 23
